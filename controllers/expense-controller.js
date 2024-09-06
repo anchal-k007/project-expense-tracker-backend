29410b58d897276ede1813e894d13c01615b3802
Anchal Kumar <anchalkmr007@gmail.com>
2024-09-06 18:12:06 +0530

    add postAddNewExpense function

--- controllers/expense-controller.js
+++ controllers/expense-controller.js
@@ -1,47 +1,74 @@
-const DUMMY_EXPENSES = [
-  {
-    paymentId: 1,
-    date: new Date(2024, 7, 29),
-    amount: 100,
-    paymentMode: "Cash",
-    reason: "Milk",
-  },
-  {
-    paymentId: 2,
-    date: new Date(2024, 7, 25),
-    amount: 150,
-    paymentMode: "Cash",
-    reason: "Eggs",
-  },
-  {
-    paymentId: 3,
-    date: new Date(2024, 7, 29),
-    amount: 249,
-    paymentMode: "UPI",
-    reason: "Food",
-  },
-  {
-    paymentId: 4,
-    date: new Date(2024, 7, 25),
-    amount: 1000,
-    paymentMode: "UPI",
-    reason: "Party",
-  },
-];
+const fs = require("fs/promises");
+const path = require("path");
 
 exports.getAllExpenses = (req, res, next) => {
-  const { year, month, date } = req.query;
-  let filteredExpenses;
-  if (!year || !month || !date) {
-    filteredExpenses = DUMMY_EXPENSES;
-  } else {
-    const createdDate = new Date(year, month, date);
-    filteredExpenses = DUMMY_EXPENSES.filter(
-      (expense) => expense.date.getTime() === createdDate.getTime()
-    );
-  }
+  const filePath = path.join(__dirname, "..", "utils", "data.json");
+  fs.readFile(filePath)
+    .then((data) => {
+      console.log("Data loaded successfully");
+      return JSON.parse(data);
+    })
+    .catch((err) => {
+      console.log("Eror in loading file");
+      console.log(err);
+    })
+    .then((DUMMY_EXPENSES) => {
+      const { year, month, date } = req.query;
+      let filteredExpenses;
+      if (!year || !month || !date) {
+        filteredExpenses = DUMMY_EXPENSES;
+      } else {
+        const createdDate = new Date(year, month, date);
+        console.log(createdDate.toISOString());
+        filteredExpenses = DUMMY_EXPENSES.filter(
+          (expense) => expense.date === createdDate.toISOString()
+        );
+      }
 
-  res.status(200).json({
-    expenses: filteredExpenses,
-  });
+      res.status(200).json({
+        expenses: filteredExpenses,
+      });
+    });
+};
+
+exports.postAddNewExpense = (req, res, next) => {
+  const { date, amount, paymentMode, reason } = req.body;
+  const newExpense = {
+    paymentId: new Date().getTime().toString(),
+    date: new Date(date),
+    amount: +amount,
+    paymentMode,
+    reason,
+  };
+  const filePath = path.join(__dirname, "..", "utils", "data.json");
+  fs.readFile(filePath)
+    .then((data) => {
+      console.log("Data loaded successfully");
+      return JSON.parse(data);
+    })
+    .catch((err) => {
+      console.log("Eror in loading file");
+      console.log(err);
+    })
+    .then((loadedExpenses) => {
+      loadedExpenses.push(newExpense);
+      fs.writeFile(
+        filePath,
+        JSON.stringify(loadedExpenses)
+      ).then(() => {
+        console.log("Data written to file successfully");
+        res.status(201).json({
+          status: "success",
+          expense: newExpense,
+        });
+      })
+      .catch(err => {
+        console.log("Error in writing data");
+        console.log(err);
+        res.status(500).json({
+          status: "error",
+          message: "internal server error"
+        });
+      })
+    });
 };
--- routes/expense-router.js
+++ routes/expense-router.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { getAllExpenses } = require("../controllers/expense-controller");
+const { getAllExpenses, postAddNewExpense } = require("../controllers/expense-controller");
  
 const router = express.Router();
 
 router.get("/all", getAllExpenses);
+
+router.post("/new", postAddNewExpense);
 
 module.exports = router;
