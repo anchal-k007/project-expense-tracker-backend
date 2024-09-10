bce682eb6ef830bd68312ddcdd74ec8b15a949f9
Anchal Kumar <anchalkmr007@gmail.com>
2024-09-11 00:03:12 +0530

    Add user authorisation to all the expense routes

--- controllers/expense-controller.js
+++ controllers/expense-controller.js
@@ -1,15 +1,18 @@
+const UserModel = require("../models/user-model");
 const ExpenseModel = require("./../models/expense-model");
 
 exports.getAllExpenses = async (req, res, next) => {
   try {
     const { year, month, date } = req.query;
+    const userId = req.userId;
     let filteredExpenses;
     if (!year || !month || !date) {
-      filteredExpenses = await ExpenseModel.find().limit(50);
+      filteredExpenses = await ExpenseModel.find({ user: userId }).limit(50);
     } else {
       const createdDate = new Date(year, month, date);
       console.log(createdDate.toISOString());
       filteredExpenses = await ExpenseModel.find({
+        user: userId,
         date: createdDate.toISOString(),
       });
     }
@@ -28,14 +31,19 @@ exports.getAllExpenses = async (req, res, next) => {
 
 exports.postAddNewExpense = async (req, res, next) => {
   const { date, amount, paymentMode, reason } = req.body;
+  const userId = req.userId;
   const newExpense = new ExpenseModel({
     date: new Date(date),
     amount: +amount,
     paymentMode,
     reason,
+    user: userId,
   });
   try {
     const createdExpense = await newExpense.save();
+    const user = await UserModel.findByIdAndUpdate(userId, {
+      $push: { expenses: createdExpense },
+    });
     res.status(201).json({
       status: "success",
       expense: createdExpense,
@@ -52,14 +60,21 @@ exports.postAddNewExpense = async (req, res, next) => {
 
 exports.deleteRemoveExpense = async (req, res, next) => {
   const expenseId = req.params.expenseId;
+  const userId = req.userId;
   try {
-    const deletedExpense = await ExpenseModel.findByIdAndDelete(expenseId);
+    const deletedExpense = await ExpenseModel.findOneAndDelete({
+      _id: expenseId,
+      user: userId,
+    });
     if (!deletedExpense) {
       return res.status(404).json({
         status: "fail",
-        message: `Did not find an expense with the id = ${expenseId}`,
+        message: `Did not find an expense with the id = ${expenseId} belonging to the user ${userId}`,
       });
     }
+    const updatedUser = await UserModel.findByIdAndUpdate(userId, {
+      $pull: { expenses: deletedExpense._id },
+    });
     res.status(204).json({
       status: "success",
       message: "Deleted the following expense",
@@ -76,6 +91,7 @@ exports.deleteRemoveExpense = async (req, res, next) => {
 };
 
 exports.putUpdateExpense = async (req, res, next) => {
+  const userId = req.userId;
   const validProperties = ["paymentMode", "amount", "date", "reason"];
   const expenseId = req.params.expenseId;
   const dataToUpdate = req.body;
@@ -85,8 +101,8 @@ exports.putUpdateExpense = async (req, res, next) => {
   );
 
   try {
-    const updatedExpense = await ExpenseModel.findByIdAndUpdate(
-      expenseId,
+    const updatedExpense = await ExpenseModel.findOneAndUpdate(
+      { _id: expenseId, user: userId },
       dataToUpdate,
       { runValidators: true, returnDocument: "after" }
     );
--- models/expense-model.js
+++ models/expense-model.js
@@ -1,29 +1,37 @@
 const mongoose = require("mongoose");
 
-const ExpenseSchema = new mongoose.Schema({
-  amount: {
-    type: Number,
-    required: [true, "An expense must have an amount"]
+const ExpenseSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: [true, "An expense must have an amount"],
+    },
+    date: {
+      type: Date,
+      required: [true, "An expense must have a date specified"],
+    },
+    paymentMode: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["Cash", "Card", "UPI"],
+        message: "{VALUE} is not a supported payment mode",
+      },
+    },
+    reason: {
+      type: String,
+      required: true,
+      minLength: 0,
+      maxLength: [80, "Reason can be at max 80 characters long"],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserModel",
+      required: true,
+    },
   },
-  date: {
-    type: Date,
-    required: [true, "An expense must have a date specified"]
-  },
-  paymentMode: {
-    type: String,
-    required: true,
-    enum: {
-      values: ["Cash", "Card", "UPI"],
-      message: "{VALUE} is not a supported payment mode"
-    }
-  },
-  reason: {
-    type: String,
-    required: true,
-    minLength: 0,
-    maxLength: [80, "Reason can be at max 80 characters long"]
-  }
-}, {versionKey: false});
+  { versionKey: false }
+);
 
 const ExpenseModel = mongoose.model("expense", ExpenseSchema);
 
