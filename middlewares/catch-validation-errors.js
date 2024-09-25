3c8c194fb5143e7863616b44494d1a05a1360878
Anchal Kumar <anchalkmr007@gmail.com>
2024-09-25 20:44:41 +0530

    update validation errors format

--- middlewares/catch-validation-errors.js
+++ middlewares/catch-validation-errors.js
@@ -4,12 +4,16 @@ const errorCreator = require("../utils/error-creator");
 module.exports = async (req, res, next) => {
   const validationErrors = validationResult(req);
   if (!validationErrors.isEmpty()) {
-    const errorObject = validationErrors.array().map((obj) => {
+    const errors = validationErrors.array().map((obj) => {
       return {
         field: obj.path,
         message: obj.msg,
       };
     });
+    const errorObject = {
+      message : "The following validation errors occurred",
+      errors,
+    }
     return next(errorCreator(errorObject, 404));
   }
   // No validation errors present
