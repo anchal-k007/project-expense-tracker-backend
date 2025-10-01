9919afed23c3204e13cb6cff63e791f950f40d4c
Anchal Kumar <anchalkmr007@gmail.com>
2025-10-01 21:27:49 +0530

    Create payment method model

--- models/model-name-constants.js
+++ models/model-name-constants.js
@@ -1,9 +1,11 @@
 const EXPENSE_MODEL_NAME = "Expense";
+const PAYMENT_METHOD_MODEL_NAME = "Payment Method";
 const TAG_MODEL_NAME = "Tag";
 const USER_MODEL_NAME = "User";
 
 module.exports = {
   EXPENSE_MODEL_NAME,
+  PAYMENT_METHOD_MODEL_NAME,
   TAG_MODEL_NAME,
   USER_MODEL_NAME,
 };
