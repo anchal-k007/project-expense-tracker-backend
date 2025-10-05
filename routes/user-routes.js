88175ded605b5f1d342fda5c65373765e7757dcc
Anchal Kumar <anchalkmr007@gmail.com>
2025-10-05 12:04:14 +0530

    Add payment-method related routes to user router

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const { isAuth } = require("./../controllers/auth-controller");
 const { getTags, postCreateTag, putUpdateTag } = require("../controllers/user-controller");
+const {
+  getPaymentMethod,
+  postCreatePaymentMethod,
+  putUpdatePaymentMethod,
+} = require("../controllers/payment-method-controller");
 
 const router = express.Router();
 
 router.route("/tags").get(isAuth, getTags).post(isAuth, postCreateTag);
 
 router.route("/tags/:tagId").put(isAuth, putUpdateTag);
+
+router.route("/payment-method").post(isAuth, postCreatePaymentMethod);
+
+router.route("/payment-method/:paymentMethodId").get(isAuth, getPaymentMethod).put(isAuth, putUpdatePaymentMethod);
 
 module.exports = router;
